76a43b0838a22f12fc3404fea61e6e6913b3a100
isoboye24 <isobarguy4real@gmail.com>
2024-12-09 18:04:19 +0100

    I implemented Category Repository

---
+++ DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private AppDBContext db;
+        public CategoryRepository(AppDBContext _db) : base(_db)
+        {
+            db = _db;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            db.Categories.Update(obj);
+        }
+    }
+}
