a192aab7e343d72e03220ab88f754831a812e039
isoboye24 <isobarguy4real@gmail.com>
2024-12-11 16:09:48 +0100

    I created Product model, created its table through EF migration.

--- DataAccess/Data/AppDBContext.cs
+++ DataAccess/Data/AppDBContext.cs
@@ -15,12 +15,87 @@ namespace DataAccess.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
                 new Category { ID = 1, Name = "Maths", DisplayOrder = 1 },
                 new Category { ID = 2, Name = "Science", DisplayOrder = 2 },
                 new Category { ID = 3, Name = "Language", DisplayOrder = 3 }
+            );
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ID = 1,
+                    Title = "Fortune of Time",
+                    Description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+                    ISBN = "SWD999001",
+                    Author = "Carlson Ben",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80,
+                },
+                new Product
+                {
+                    ID = 2,
+                    Title = "How to avoid war",
+                    Description = "etetur sadipscing elitrut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+                    ISBN = "SWD999002",
+                    Author = "Benjamin Franklin",
+                    ListPrice = 119,
+                    Price = 109,
+                    Price50 = 100,
+                    Price100 = 95,
+                },
+                new Product
+                {
+                    ID = 3,
+                    Title = "Religious Freedom",
+                    Description = "Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+                    ISBN = "SWD999003",
+                    Author = "Iminabo Tombo",
+                    ListPrice = 89,
+                    Price = 80,
+                    Price50 = 75,
+                    Price100 = 70,
+                },
+                new Product
+                {
+                    ID = 4,
+                    Title = "Marital Success",
+                    Description = "Dolor sit ametaliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+                    ISBN = "SWD999004",
+                    Author = "Kroma Belema",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price50 = 45,
+                    Price100 = 40,
+                },
+                new Product
+                {
+                    ID = 5,
+                    Title = "Financial Success",
+                    Description = "At vero eos et accusam et justo duo dolores et ea rebum.",
+                    ISBN = "SWD999005",
+                    Author = "Biokpo Alabo",
+                    ListPrice = 99,
+                    Price = 70,
+                    Price50 = 65,
+                    Price100 = 60,
+                },
+                new Product
+                {
+                    ID = 6,
+                    Title = "Starts of a Firm",
+                    Description = "Lnvidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+                    ISBN = "SWD999006",
+                    Author = "Endurance Goodwill",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80,
+                }
             );
         }
     }
---
+++ DataAccess/Migrations/20241211150630_AddProductToTableDB.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductToTableDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ID", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Carlson Ben", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.", "SWD999001", 99.0, 90.0, 80.0, 85.0, "Fortune of Time" },
+                    { 2, "Benjamin Franklin", "etetur sadipscing elitrut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.", "SWD999002", 119.0, 109.0, 95.0, 100.0, "How to avoid war" },
+                    { 3, "Iminabo Tombo", "Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.", "SWD999003", 89.0, 80.0, 70.0, 75.0, "Religious Freedom" },
+                    { 4, "Kroma Belema", "Dolor sit ametaliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.", "SWD999004", 55.0, 50.0, 40.0, 45.0, "Marital Success" },
+                    { 5, "Biokpo Alabo", "At vero eos et accusam et justo duo dolores et ea rebum.", "SWD999005", 99.0, 70.0, 60.0, 65.0, "Financial Success" },
+                    { 6, "Endurance Goodwill", "Lnvidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.", "SWD999006", 99.0, 90.0, 80.0, 85.0, "Starts of a Firm" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
