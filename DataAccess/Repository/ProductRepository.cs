d9e32c196c1d86c61a0270c386c3d5c3dc775d1a
isoboye24 <isobarguy4real@gmail.com>
2024-12-11 16:16:12 +0100

    I implemented Product Repository

---
+++ DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private AppDBContext db;
+        public ProductRepository(AppDBContext _db) : base(_db)
+        {
+            db = _db;
+        }
+
+        public void Update(Product obj)
+        {
+            db.Products.Update(obj);
+        }
+    }
+}
