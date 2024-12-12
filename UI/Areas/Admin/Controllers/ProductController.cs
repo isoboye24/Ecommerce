fb323479668edcdbf2e2840461e753bec8d497df
isoboye24 <isobarguy4real@gmail.com>
2024-12-12 12:50:45 +0100

    I created product controller, their actions and their action views. I also created navbar link to the product index view.

---
+++ UI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public ProductController(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = unitOfWork.Product.GetAll().ToList();
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product) 
+        { 
+            return View();
+        }
+        public IActionResult Create(int? id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            return View();
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            return View();
+        }
+    }
+}
