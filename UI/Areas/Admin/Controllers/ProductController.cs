b6b38b5ba9ae7aa5775b9dc89fde436d24803ae4
isoboye24 <isobarguy4real@gmail.com>
2024-12-12 13:57:14 +0100

    I implemented the Product CRUD actions and designed their views.

--- UI/Areas/Admin/Controllers/ProductController.cs
+++ UI/Areas/Admin/Controllers/ProductController.cs
@@ -23,26 +23,70 @@ namespace UI.Areas.Admin.Controllers
         }
         [HttpPost]
         public IActionResult Create(Product product) 
-        { 
+        {
+            if (product.Title == product.ListPrice.ToString())
+            {
+                ModelState.AddModelError("name", "The Product price list cannot exactly match the Title.");
+            }
+            if (ModelState.IsValid)
+            {
+                unitOfWork.Product.Add(product);
+                unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
             return View();
         }
-        public IActionResult Create(int? id)
+        public IActionResult Edit(int? id)
         {
-            return View();
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product productItem = unitOfWork.Product.Get(x => x.ID == id);
+            if (productItem == null)
+            {
+                return NotFound();
+            }
+            return View(productItem);
         }
         [HttpPost]
         public IActionResult Edit(Product product)
         {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.Product.Update(product);
+                unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
             return View();
         }
         public IActionResult Delete(int? id)
         {
-            return View();
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product productItem = unitOfWork.Product.Get(x => x.ID == id);
+            if (productItem == null)
+            {
+                return NotFound();
+            }
+            return View(productItem);
         }
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePost(int? id)
         {
-            return View();
+            Product? productItem = unitOfWork.Product.Get(x => x.ID == id);
+            if (productItem == null)
+            {
+                return NotFound();
+            }
+            unitOfWork.Product.Remove(productItem);
+            unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
