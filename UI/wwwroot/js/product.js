62ab4b80bafca629ab0ac5ad64fbc49a51941a97
isoboye24 <isobarguy4real@gmail.com>
2024-12-18 23:55:15 +0100

    I loaded the data in a table using dataTable

---
+++ UI/wwwroot/js/product.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall'},
+        "columns": [
+            { data: 'title', "width": "15%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'price', "width": "15%" },
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "15%" }
+        ]
+    });
+}
