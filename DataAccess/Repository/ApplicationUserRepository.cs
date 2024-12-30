1f10ca08cdc803157681bf2dde80c897a116efe4
isoboye24 <isobarguy4real@gmail.com>
2024-12-30 01:17:08 +0100

    I added Application user to the Repository.

---
+++ DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private AppDBContext db;
+        public ApplicationUserRepository(AppDBContext _db) : base(_db)
+        {
+            db = _db;
+        }
+
+        public void Update(ApplicationUser obj)
+        {
+            db.ApplicationUsers.Update(obj);
+        }
+    }
+}
