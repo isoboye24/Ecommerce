49b7e02f5beb943b5459afaaf3a8364f2815c2a3
isoboye24 <isobarguy4real@gmail.com>
2024-12-31 02:14:59 +0100

    I added OrderHeader and OrderDetail to Repository.

---
+++ DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private AppDBContext db;
+        public OrderDetailRepository(AppDBContext _db) : base(_db)
+        {
+            db = _db;
+        }
+
+        public void Update(OrderDetail obj)
+        {
+            db.OrderDetails.Update(obj);
+        }
+    }
+}
---
+++ DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private AppDBContext db;
+        public OrderHeaderRepository(AppDBContext _db) : base(_db)
+        {
+            db = _db;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+            db.OrderHeaders.Update(obj);
+        }
+    }
+}
