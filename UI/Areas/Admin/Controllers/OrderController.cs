343cd584f8ac0bb4caef7de8646997252a81e74d
isoboye24 <isobarguy4real@gmail.com>
2025-01-09 05:04:07 +0100

    I created OrderViewModel and OrderController

---
+++ UI/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Repository;
+using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace UI.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public OrderController(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaderList = unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = orderHeaderList });
+        }
+        #endregion
+    }
+}
