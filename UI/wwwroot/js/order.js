6961a081c31c5770716c09223733a401a83fd854
isoboye24 <isobarguy4real@gmail.com>
2025-01-10 23:58:52 +0100

    The x.Id returns null and count is 0.

--- UI/wwwroot/js/order.js
+++ UI/wwwroot/js/order.js
@@ -23,20 +23,20 @@ $(document).ready(function () {
             }
         }
     }
-})
+});
 
 function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
-            { "data": 'orderHeaderID', "width": "5%" },
-            { "data": 'name', "width": "25%" },
-            { "data": 'phoneNumber', "width": "15%" },
-            { "data": 'applicationUser.email', "width": "25%" },
-            { "data": 'orderStatus', "width": "10%" },
-            { "data": 'orderTotal', "width": "10%" },
+            { data: 'orderHeaderID', "width": "5%" },
+            { data: 'name', "width": "25%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'applicationUser.email', "width": "25%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
             {
-                data: 'id',
+                data: 'orderHeaderID',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
                         <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2" ><i class="bi bi-pencil-square"></i> </a>                        
