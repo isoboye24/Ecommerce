bc49a88f313ede98b29b9f59f7cc60648ed3ee5e
isoboye24 <isobarguy4real@gmail.com>
2025-01-22 09:53:32 +0100

    I added ProductImage to the Repository.

---
+++ DataAccess/Repository/ProductImageRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository
+    {
+        private AppDBContext db;
+        public ProductImageRepository(AppDBContext _db) : base(_db)
+        {
+            db = _db;
+        }
+
+        public void Update(ProductImage obj)
+        {
+            db.ProductImages.Update(obj);
+        }
+    }
+}
